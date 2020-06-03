e8121e25b8fb7fc1b4b8f942ee46888729d56974
Nick Young <youngnicholas228@gmail.com>
2020-06-03 14:04:57 -0700

    environment variables, client-side queries, and solidified graphql

--- client/components/laundry/LaundryScreen.js
+++ client/components/laundry/LaundryScreen.js
@@ -1,9 +1,15 @@
 import * as React from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import LaundryCard from "./LaundryCard";
-import { AntDesign, Ionicons } from '@expo/vector-icons';
-import { LAUNDRY_DATA } from "../../assets/dummydata/laundry/endpoint"
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { LAUNDRY_DATA } from "../../data/dummydata/laundry/endpoint";
 
 export default class LaundryScreen extends React.Component {
   constructor(props) {
@@ -23,67 +29,82 @@ export default class LaundryScreen extends React.Component {
   }
 
   // Called when a card is starred or unstarred
-  onStarChanged = (card) => {
+  onStarChanged = card => {
     const starred = this.state.starred;
     if (starred.has(card)) {
       starred.delete(card);
     } else {
       starred.add(card);
     }
-    this.setState(() => ({})) // re-renders LaundryScreen
+    this.setState(() => ({})); // re-renders LaundryScreen
   };
 
   // Called on search bar text change
-  onTextChanged = (text) => {
+  onTextChanged = text => {
     let emptySearchBar = true;
     let newSuggestions = [];
     if (text.length > 0) {
       emptySearchBar = false;
       newSuggestions = Object.keys(this.cards)
         .sort()
-        .filter((v) => (v.toLowerCase().indexOf(text.toLowerCase()) != -1 ));
+        .filter(v => v.toLowerCase().indexOf(text.toLowerCase()) != -1);
     }
     this.setState(() => ({ emptySearchBar, suggestions: newSuggestions }));
   };
 
   // Returns scrolling card view, given list of cards (keys) to be rendered
-  mapToCards (toMap) {
+  mapToCards(toMap) {
     return (
       <ScrollView>
-        {toMap.map((card) =>
+        {toMap.map(card => (
           <LaundryCard
             key={card}
             card={this.cards[card]}
             starred={this.state.starred.has(card)}
-            starAction={() => this.onStarChanged(card)} />)}
+            starAction={() => this.onStarChanged(card)}
+          />
+        ))}
       </ScrollView>
-      )
+    );
   }
 
   // Returns search bar results, starred laundry rooms, or no results message
-  renderSuggestions () {
+  renderSuggestions() {
     const { emptySearchBar, suggestions } = this.state;
-    const starredArr = Array.from(this.state.starred).sort()
+    const starredArr = Array.from(this.state.starred).sort();
     if (suggestions.length === 0) {
       if (emptySearchBar) {
         if (starredArr.length === 0) {
-          return <Text style={styles.textCentered}>No starred laundry rooms yet.</Text>;
+          return (
+            <Text style={styles.textCentered}>
+              No starred laundry rooms yet.
+            </Text>
+          );
         }
         return this.mapToCards(starredArr);
       }
       return <Text style={styles.textCentered}>No search results found.</Text>;
     }
-    
+
     return this.mapToCards(suggestions);
   }
 
   // Returns search bar's clear button when there is text in the search box
-  crossHandler () {
+  crossHandler() {
     const { emptySearchBar } = this.state;
     if (!emptySearchBar) {
       return (
-        <TouchableOpacity onPress={() => {this.textInput.clear(); this.onTextChanged("")}}>
-          <AntDesign style={styles.crossIcon} name="close" size={24} color="#A9A9A9" />
+        <TouchableOpacity
+          onPress={() => {
+            this.textInput.clear();
+            this.onTextChanged("");
+          }}>
+          <AntDesign
+            style={styles.crossIcon}
+            name='close'
+            size={24}
+            color='#A9A9A9'
+          />
         </TouchableOpacity>
       );
     }
@@ -93,15 +114,23 @@ export default class LaundryScreen extends React.Component {
     return (
       <View style={styles.screen}>
         <View style={styles.searchBar}>
-          <Ionicons style={styles.searchIcon} name="ios-search" size={24} color="gray" />
+          <Ionicons
+            style={styles.searchIcon}
+            name='ios-search'
+            size={24}
+            color='gray'
+          />
           <TextInput
-          style={styles.textInput}
-          ref={input => { this.textInput = input }}
-          placeholder="Search laundry"
-          onChangeText={(text) => this.onTextChanged(text)} />
+            style={styles.textInput}
+            ref={input => {
+              this.textInput = input;
+            }}
+            placeholder='Search laundry'
+            onChangeText={text => this.onTextChanged(text)}
+          />
           {this.crossHandler()}
         </View>
-        
+
         {this.renderSuggestions()}
       </View>
     );
@@ -123,7 +152,7 @@ const styles = StyleSheet.create({
   },
   textCentered: {
     marginTop: 20,
-    alignSelf: 'center',
+    alignSelf: "center",
     fontSize: 18,
     color: "#9C9C9C",
   },
@@ -134,22 +163,22 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingLeft: 12,
     margin: 12,
-    width: '88%',
-    flexDirection: 'row',
-    alignSelf: 'center',
+    width: "88%",
+    flexDirection: "row",
+    alignSelf: "center",
 
     // shadows for ios
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowRadius: 2,
     shadowOpacity: 0.25,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     shadowOffset: {
-        width: 0,
-        height: 1
+      width: 0,
+      height: 1,
     },
 
     // shadows for android
-    elevation: 5
+    elevation: 5,
   },
   searchIcon: {
     marginTop: 2,
---
+++ client/data/queries/laundryQueries.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const { brownapi } = require("../constants/urls");
+require("dotenv").config();
+
+// Returns all laundry rooms.
+const getLaundryRooms = async () => {
+  let endpoint = brownapi + `laundry/rooms?client_id=${process.env.CLIENT_ID}`;
+  let { data } = await axios.get(endpoint).catch(e => console.log(e));
+  return data;
+};
+
+// Returns laundry room with given id.
+const getLaundryRoom = async (room_id, details) => {
+  let endpoint =
+    brownapi + `laundry/rooms/${room_id}?client_id=${process.env.CLIENT_ID}`;
+  endpoint += details ? "&get_status=true" : "";
+  let { data } = await axios.get(endpoint).catch(e => console.log(e));
+  return data.result;
+};
+
+// Returns laundry machine with given id.
+const getLaundryMachine = async (room_id, machine_id, details) => {
+  let endpoint =
+    brownapi +
+    `laundry/rooms/${room_id}/machines/${machine_id}?client_id=${process.env.CLIENT_ID}`;
+  endpoint += details ? "&get_status=true" : "";
+  let { data } = await axios.get(endpoint).catch(e => console.log(e));
+  return data.result;
+};
+
+module.exports = {
+  getLaundryRooms: getLaundryRooms,
+  getLaundryRoom: getLaundryRoom,
+  getLaundryMachine: getLaundryMachine,
+};
