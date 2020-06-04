02c55645ac01a787e1820620bd18933c0f5541ac
Nick Young <youngnicholas228@gmail.com>
2020-06-03 17:36:16 -0700

    mapdata and map view

--- client/components/map/MapScreen.js
+++ client/components/map/MapScreen.js
@@ -1,13 +1,77 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Button, Icon } from "react-native-elements";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { initialRegion, mapStyle } from "./MapConfig";
+import { BUILDINGS } from "../../data/mapdata/parsedBuildings";
+
+// TODO: Change marker color, make it so that user can select which marker categories to render.
+// TODO: figure out marker categories.
+const ParseMarkers = () => {
+  return (
+    <>
+      {BUILDINGS.map(e => (
+        <Marker
+          coordinate={{ latitude: e.latitude, longitude: e.longitude }}
+          key={e.id}
+          title={e.name}
+          pinColor="blue"
+        ></Marker>
+      ))}
+    </>
+  );
+};
 
 export default function MapScreen() {
   return (
-    <View>
-      <Text>Helllo World!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{ ...StyleSheet.absoluteFillObject }}
+          initialRegion={initialRegion}
+        >
+          <ParseMarkers />
+        </MapView>
+        <Button
+          buttonStyle={styles.button}
+          icon={() => (
+            <Icon reverse name="map-marker" type="font-awesome" color="red" />
+          )}
+        />
+        <Button
+          buttonStyle={styles.button}
+          icon={() => (
+            <Icon
+              reverse
+              name="location-arrow"
+              type="font-awesome"
+              color="red"
+            />
+          )}
+        />
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "flex-start",
+  },
+  map: {
+    flex: 1,
+  },
+  button: {
+    marginRight: 20,
+    marginTop: 20,
+    backgroundColor: "red",
+    width: 50,
+    height: 50,
+  },
+});
 
 MapScreen.navigationOptions = {
   header: null,
---
+++ client/data/mapdata/parseBuildings.py
@@ -0,0 +1,21 @@
+import csv
+with open('buildings.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    print("[", end="")
+    for row in csv_reader:
+        print("{", end="")
+        print("abbr:\"" + row[0] + "\",", end="")
+        print("id:" + row[1] + ",", end="")
+        print("name:\"" + row[2] + "\",", end="")
+        print("site:\"" + row[3] + "\",", end="")
+        print("use:\"" + row[4] + "\",", end="")
+        print("status:\"" + row[5] + "\",", end="")
+        print("address_1:\"" + row[6] + "\",", end="")
+        print("address_2:\"" + row[7] + "\",", end="")
+        print("owned:\"" + row[8] + "\",", end="")
+        print("type:\"" + row[9] + "\",", end="")
+        print("coordinate:\"" + row[10] + "\",", end="")
+        print("latitude:" + row[11] + ",", end="")
+        print("longitude:" + row[12] + ",", end="")
+        print("},", end="")
+    print("]", end="")
