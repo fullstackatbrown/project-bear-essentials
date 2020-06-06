4629863c0ca29964a6acd307e4d6b9a154f25871
ems-wu <61918373+ems-wu@users.noreply.github.com>
2020-06-06 12:54:17 -1000

    dining setup

--- client/data/dummydata/dining/endpoint.js
+++ client/data/dummydata/dining/endpoint.js
@@ -1,2 +1,32 @@
 // TODO: Make dummy data
-export const DINING = {};
+export const DINING_HOURS = {
+    "SHARPE REFECTORY" :{
+        tite: "Sharpe Refectory Hours",
+        date: "2020-06-01",
+        hours: [
+          {
+            start_time: "07:30",
+            end_time: "21:00",
+            id: "1531",
+          }
+        ]
+      },
+  };
+
+  export const DINING_MENU = {
+      "ANDREWS COMMONS" :{
+        tite: "Andrews Commons Menu",
+        date: "2020-06-01",
+        hours: [
+          {
+            start_time: "08:00",
+            end_time: "22:00",
+            id: "1533",
+            entree: ["Scrambled Eggs", "Turkey Sausage", "Ham Slices"],
+            dessert: ["Glazed Donuts", "Fudge Brownies", "Sugar Cookies"],
+          }
+        ]
+      }     
+  };
+
+export { DINING_DATA };
