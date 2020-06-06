75a19f093b843c7d24031c7468d023ca39578cd6
Leon Jiang <leon.jiangy@gmail.com>
2020-06-05 18:19:11 -0700

    update colors.js

--- client/constants/Colors.js
+++ client/constants/Colors.js
@@ -1,14 +1,21 @@
 const tintColor = '#2f95dc';
 
 export default {
-  tintColor,
+  tintColor: "#cc0200",
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
+  tabBar: '#fff',
   errorBackground: 'red',
   errorText: '#fff',
   warningBackground: '#EAEB5E',
   warningText: '#666804',
   noticeBackground: tintColor,
   noticeText: '#fff',
+  bearBrown: "#7d5125",
+  starYellow: "#ffef26",
+  danger: "db3737",
+  warning: "#d9822b",
+  success: "#0f9960",
+  inactiveIcon: "ccc",
+  activeIcon: "#949494"
 };
